e89af5b03d92ab449d365a7478b95c1343ef0364
HunterMillerc <miller.hunterc@gmail.com>
2020-05-31 22:25:05 -0500

    Added Game to folder

---
+++ FoodSupplyGame/Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 2f;
+
+    Vector3 forward, right;
+
+    private void Start()
+    {
+        forward = Camera.main.transform.forward;
+    }
+}
