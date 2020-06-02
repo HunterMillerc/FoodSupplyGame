a5bec9845d95907be66795d01d9e9308b0034830
HunterMillerc <miller.hunterc@gmail.com>
2020-06-01 21:14:56 -0500

    Made level01 Need to create movement scripts and others

---
+++ FoodSupplyGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(40, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddTorque(0, 0, -40);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddTorque(0, 0, 40);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(-40, 0, 0);
+        }
+
+        
+    }
+}
