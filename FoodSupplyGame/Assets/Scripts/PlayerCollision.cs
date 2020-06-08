61edeaed903cd691ed582968e5b489702d346bb7
HunterMillerc <miller.hunterc@gmail.com>
2020-06-08 14:07:52 -0500

    Added GameManager script and object

--- FoodSupplyGame/Assets/Scripts/PlayerCollision.cs
+++ FoodSupplyGame/Assets/Scripts/PlayerCollision.cs
@@ -5,7 +5,27 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public MyPlayerMovement playerMovement;
-    private string objectHit;
+    public GameManager gameManager;
+    bool isFoodPicked;
 
-   
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "FoodTag")
+        {
+            isFoodPicked = true;
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.tag == "Finish")
+        {
+            if(isFoodPicked == true)
+            {
+                gameManager.CompleteLevel();
+            }
+            else
+            {
+                Debug.Log("need to get food");
+            }
+        }
+    }
 }
