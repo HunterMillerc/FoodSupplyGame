82bef975023242ad1fffad13c7edb739bfd75cb7
HunterMillerc <miller.hunterc@gmail.com>
2020-07-02 12:47:11 -0500

    Revert "Revert "On screen pizza UI""
    This reverts commit ba529f7fe60bcc97e1cea9c715b68052c7f799e0.

--- FoodSupplyGame/Assets/Scripts/PizzaHit.cs
+++ FoodSupplyGame/Assets/Scripts/PizzaHit.cs
@@ -8,11 +8,15 @@ public class PizzaHit : MonoBehaviour
     public GameObject Pizza;
 
     public bool foodPickedUp = false;
+    public GameObject PizzaNotCollected;
+    public GameObject PizzaCollected;
 
     private void Start()
     {
         FoodCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider>();
         Pizza = GameObject.FindGameObjectWithTag("FoodTag");
+        PizzaNotCollected.SetActive(true);
+        PizzaCollected.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,6 +25,8 @@ public class PizzaHit : MonoBehaviour
         {
             foodPickedUp = true;
             Destroy(Pizza);
+            PizzaNotCollected.SetActive(false);
+            PizzaCollected.SetActive(true);
         }
     }
 }
